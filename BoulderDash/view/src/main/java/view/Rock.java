e568681b5a7243a22a6d6e7c1e72b2f4729c6446
remyCESI <remy.vancamp@viacesi.fr>
2017-06-23 10:26:25 +0200

    javadoc MAJ 10:26

--- BoulderDash/view/src/main/java/view/Rock.java
+++ BoulderDash/view/src/main/java/view/Rock.java
@@ -4,12 +4,27 @@ import java.util.TimerTask;
 
 public class Rock extends TimerTask{
 	
+	/*
+	 * 
+Attribute from the Map aggregation with Rock.
+	 */
 	private Map m;
 	
+	/*
+	 * @param m 
+	 * 	
+Attribute of aggregation Map rock   
+	 */
 	public Rock(Map m) {
 		this.m = m;
 	}
 	
+	/*
+	 * 
+	 * @see java.util.TimerTask#run()
+	 * We launch the thread allowing to fall the cascade rocks under condition that there is nothing under it.
+	 * 
+	 */
 	public void run() {
 		for(int x = 0; x < 40; x++) {
 			for(int y = 20; y>0; y--) {
@@ -25,6 +40,15 @@ public class Rock extends TimerTask{
 		}
 	}
 
+	/*
+	 * @param x,y 
+	 * 	They are localization attributes
+	 * 
+	 * 
+Allows the element to cascade if access is possible.
+	 * 
+	 */
+	
 	private void cascade(int x, int y) {
 		if (m.getMap(x+1, y).equals("_") && m.getMap(x+1, y+1).equals("_")) {
 			m.setMap(x, y, "_");
